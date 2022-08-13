2dbf4f2d252c5bbd57d4f6bd10fc894d06e3fa70
Ranastar88 <marcosilletti88@gmail.com>
2022-08-13 16:14:40 +0000

    init 2

--- waterpolostat-app/src/app/app-routing.module.ts
+++ waterpolostat-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NuovaPartitaComponent } from './partita/nuova-partita/nuova-partita.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'partita/nuova', component: NuovaPartitaComponent },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
