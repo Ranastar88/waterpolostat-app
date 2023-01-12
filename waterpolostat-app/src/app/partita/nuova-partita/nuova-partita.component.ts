dbd12e6d371920a8859706a3f372e2944607699e
Ranastar88 <marcosilletti88@gmail.com>
2023-01-12 16:35:48 +0000

    form partita

--- waterpolostat-app/src/app/partita/nuova-partita/nuova-partita.component.ts
+++ waterpolostat-app/src/app/partita/nuova-partita/nuova-partita.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Partita } from 'src/app/shared/models/partita';
 
 @Component({
@@ -8,18 +8,24 @@ import { Partita } from 'src/app/shared/models/partita';
   styleUrls: ['./nuova-partita.component.sass']
 })
 export class NuovaPartitaComponent implements OnInit {
-  public fg!: FormGroup;
+  public fg: FormGroup = this.fb.group((dataItem: Partita) => ({
+    id: dataItem.id,
+    nomeSquadraCasa: [dataItem.nomeSquadraCasa, [Validators.required, Validators.maxLength(100)]],
+    nomeSquadraOspiti: [dataItem.nomeSquadraOspiti, [Validators.required, Validators.maxLength(100)]],
+    luogo: [dataItem.luogo, [Validators.maxLength(100)]],
+    campionato: [dataItem.campionato, [Validators.maxLength(100)]],
+  }));
 
   constructor(public fb: FormBuilder) { 
 
   }
+  get nomeSquadraCasa():FormControl  { return this.fg.get('nomeSquadraCasa') as FormControl; }
+  get nomeSquadraOspiti() { return this.fg.get('nomeSquadraOspiti') as FormControl; }
+  get luogo() { return this.fg.get('luogo') as FormControl; }
+  get campionato() { return this.fg.get('campionato') as FormControl; }
 
   ngOnInit(): void {
-    this.fg = this.fb.group((dataItem: Partita) => ({
-      id: dataItem.id,
-      nomeSquadraCasa: [dataItem.nomeSquadraCasa, [Validators.required, Validators.maxLength(100)]],
-      nomeSquadraOspiti: [dataItem.nomeSquadraOspiti, [Validators.required, Validators.maxLength(100)]]
-    }));
   }
+
 
 }
